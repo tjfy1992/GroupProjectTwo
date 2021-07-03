5e601b4db52087d1cd8e037dfeb11bacd49a1017
lsnuwafi <lsnuwafi@gmail.com>
2021-07-03 12:05:35 -0400

    add disabled

--- Frontend/timesheet-app/src/components/timesheet/timesheet.jsx
+++ Frontend/timesheet-app/src/components/timesheet/timesheet.jsx
@@ -47,6 +47,10 @@ const columns = [
     },
 ];
 
+function disabledDate(current) {
+    // Can not select days before today and today
+    return ((current.toDate().getDay() != 0 && current.toDate().getDay() != 6));
+}
 
 function getEndDate(_today = new Date()) {
     var today = _today
@@ -60,6 +64,7 @@ const zeroHourSelect = <Select style={{ width: 120 }} defaultValue={0.00} disabl
 const TimeList = []
 const vacationOptions = []
 const approveOptionList = ['Approved timesheet', 'unapproved timesheet'];
+
 //const approveOptionList = ['Approved timesheet', 'unapproved timesheet'];
 const workHourData = [0.00, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00, 8.00, 9.00, 10.00, 11.00, 12.00, 13.00, 14.00, 15.00, 16.00, 17.00, 18.00, 19.00, 20.00, 21.00, 22.00, 23.00, 24.00]
 
@@ -96,14 +101,22 @@ export default class Timesheet extends Component {
     }
 
 
-    weekDayworkHourSelect = (rowId) => {return <Select style={{ width: 120 }} defaultValue={0.00} onChange={() => this.weekDayworkHourChange(rowId)}>{workHourData.map(item =>
+    weekDayworkHourSelect = (rowId) => {return <Select style={{ width: 120 }} defaultValue={8} onChange={() => this.weekDayworkHourChange(rowId)}>{workHourData.map(item =>
         <Option value={item}>{item}</Option>)}
         </Select>
     }
 
-    weekDayStartSelect = (rowId) => {return <Select style={{ width: 120 }} defaultValue={'N/A'}>{TimeList}</Select>}
-    weekDayEndSelect = (rowId) => {return <Select style={{ width: 120 }} defaultValue={'N/A'}>{TimeList}</Select>}
-    
+    weekDayStartSelect = (rowId) => { return <Select style={{ width: 120 }} defaultValue={9}>{
+        workHourData.map(item => 
+        <Option value={item}>{item}</Option>)}
+    </Select>
+    }
+
+    weekDayEndSelect = (rowId) => { return <Select style={{ width: 120 }} defaultValue={17}>{
+        workHourData.map(item =>
+        <Option value={item}>{item}</Option>)}
+    </Select>
+    }
     
 
     dataPickerChange(value) {
@@ -114,11 +127,11 @@ export default class Timesheet extends Component {
     }
 
     updateDateArray() {
+        console.log("updating arr ", this.state.endDate)
         let today = new Date()
         let arr = []
         let i = 1
         let monToday = moment(today).format('MM/DD/YYYY');
-
         let momentEndDate = moment(this.state.endDate).format('MM/DD/YYYY');
         if (moment(momentEndDate).diff(moment(monToday), 'days') > 7) {
             console.log("more than!!!")
@@ -129,9 +142,24 @@ export default class Timesheet extends Component {
         while (monToday <= momentEndDate) {
             console.log("i is: ", i)
             console.log(today.getDay())
-            var startSelector = (today.getDay() == 6 || today.getDay() == 0) ? NASelect : this.weekDayStartSelect(i - 1)
-            var endSelector = (today.getDay() == 6 || today.getDay() == 0) ? NASelect : this.weekDayEndSelect(i - 1)
-            var workHourSelect = (today.getDay() == 6 || today.getDay() == 0) ? zeroHourSelect : this.weekDayworkHourSelect(i - 1)
+            let day = today.getDay();
+            let startMeta = 9
+            let endMeta = 17
+            let workHourMeta = 8
+            let startSelector = (() => {return this.weekDayStartSelect(i - 1)})()
+            let workHourSelect = (() => {return this.weekDayworkHourSelect(i - 1)})()
+            let endSelector = (() => {return this.weekDayEndSelect(i - 1)})()
+            
+
+            if (day == 6 || day == 0) {
+                startSelector = NASelect
+                endSelector = NASelect
+                workHourSelect = zeroHourSelect
+                startMeta = 0
+                endMeta = 0
+                workHourMeta = 0
+            }
+
             arr.push({
                 key: i,
                 day: moment(today).format('dddd'),
@@ -140,10 +168,12 @@ export default class Timesheet extends Component {
                 endingTime: endSelector,
                 totalHours: workHourSelect,
                 floatingDay: ((i) => {return this.floatCheckBox(i - 1)})(i),
-                //floatingDay: ((i) => {return <Checkbox checked={this.state.rows[i] == undefined ? true : false} onChange={(e) => this.checkBoxChanged(e, i , 1)}></Checkbox>})(i), 
                 holiday: this.holidayCheckBox(i - 1),
                 vacation: this.vacationcheckBox(i - 1),
-              //  holidayMeta: 0
+                holidayMeta: 0,
+                startTimeMeta: startMeta,
+                endTimeMeta: endMeta,
+                workHourMeta: workHourMeta
             })
             i++;
             today.setDate(today.getDate() + 1);
@@ -161,6 +191,7 @@ export default class Timesheet extends Component {
             this.updateDateArray()
         }
         if (this.state.rows != prevState.rows) {
+            console.log(this.state.rows)
             // this.state.rows.forEach( (value, index) => {
 
             // }
@@ -184,7 +215,7 @@ export default class Timesheet extends Component {
         return (
             <div>
                 <p>This is timesheet tab</p>
-                Week Engding: <DatePicker defaultValue={moment(moment(this.state.endDate).format(dateFormat), dateFormat)} format={dateFormat} onChange={value => this.dataPickerChange(value)} />
+                Week Engding: <DatePicker defaultValue={moment(moment(this.state.endDate).format(dateFormat), dateFormat)} format={dateFormat} disabledDate={disabledDate} onChange={value => this.dataPickerChange(value)} />
 
                 Total Billing Hours:
                 Tolal Compensated Hours:
