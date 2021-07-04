a390ae6e5c690e5e8cde7bef1120b7707dd8facc
Yan Liu <jamesliu6858@gmail.com>
2021-07-04 06:45:16 -0400

    Add profile data transfer functions

--- Frontend/timesheet-app/src/components/profile/profile.jsx
+++ Frontend/timesheet-app/src/components/profile/profile.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './profile.css';
 import { UserOutlined } from '@ant-design/icons';
+import axios from 'axios'
 
 import { Form, Input, Button, Typography, Avatar } from 'antd';
 
@@ -8,53 +9,110 @@ import { Form, Input, Button, Typography, Avatar } from 'antd';
 
 export default class Profile extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            phone: '',
+            email: '',
+            address: ''
+        };
+    }
+
+    testGet = () => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:9000/core/test/profile',
+        })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    testData = (e) => {
+        e.preventDefault();
+        this.testGet();
+    };
+
+    saveUpdate = (e) => {
+        e.preventDefault();
+        axios({
+            method: 'post',
+            url: 'http://localhost:9000/core/test/updateprofile',
+            data: {
+                phone: this.state.phone,
+                email: this.state.email,
+                address: this.state.address
+            }
+            })
+            .then((response) => {
+                localStorage.setItem('phone', response.data.user.phone);
+                localStorage.setItem('email', response.data.user.email);
+                localStorage.setItem('address', response.data.user.address);
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error)
+        })
+    };
+
+    handlePhoneChange = (e) => {
+        this.setState({phone: e.target.value});
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value});
+    }
+
+    handleAddressChange = (e) => {
+        this.setState({address: e.target.value});
+    }
+
     render() {
 
         return (
             <div style={{textAlign: "left"}}>
                 <Avatar size={64} icon={<UserOutlined />} />
-                <Form
-                    labelCol={{ span: 9 }}
-                    wrapperCol={{ span: 5, offset: 9 }}
-                    layout="horizontal">
-
-                        <Form.Item>
-                            <Title level={5}>Contact</Title>
-                        </Form.Item>
-                        <Form.Item>
-                            <Input placeholder="Phone Number" />
-                        </Form.Item>
+                <Form labelCol={{ span: 9 }} wrapperCol={{ span: 5, offset: 9 }} layout="horizontal">
+                    <Form.Item>
+                        <Title level={5}>Contact</Title>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Phone Number" onChange={this.handlePhoneChange} />
+                    </Form.Item>
                         
-                        <Form.Item>
-                            <Input placeholder="E-mail" />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input.TextArea rows={4} placeholder="Address"/>
-                        </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="E-mail" onChange={this.handleEmailChange} />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input.TextArea rows={4} placeholder="Address" onChange={this.handleAddressChange} />
+                    </Form.Item>
 
-                        <Form.Item>
-                            <Title level={5}>Emergency Contact 1</Title>
-                        </Form.Item>
-                        <Form.Item>
-                            <Input placeholder="Name" />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input placeholder="Phone Number" />
-                        </Form.Item>
+                    <Form.Item>
+                        <Title level={5}>Emergency Contact 1</Title>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Name" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Phone Number" />
+                    </Form.Item>
                         
-                        <Form.Item>
-                            <Title level={5}>Emergency Contact 2</Title>
-                        </Form.Item>
-                        <Form.Item>
-                            <Input placeholder="Name" />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input placeholder="Phone Number" />
-                        </Form.Item>
+                    <Form.Item>
+                        <Title level={5}>Emergency Contact 2</Title>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Name" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input placeholder="Phone Number" />
+                    </Form.Item>
 
-                        <Form.Item style={{textAlign: "center"}}>
-                            <Button type="primary" htmlType="submit" className="login-form-button">Save</Button>
-                        </Form.Item>
+                    <Form.Item style={{textAlign: "center"}}>
+                        <Button type="primary" htmlType="submit" className="login-form-button" onClick={this.saveUpdate}>Save</Button>
+                    </Form.Item>
                 </Form>
             </div>
         )
--- microservice/gp2application/core-1/src/main/java/com/example/core1/controller/TestController.java
+++ microservice/gp2application/core-1/src/main/java/com/example/core1/controller/TestController.java
@@ -2,6 +2,9 @@ package com.example.core1.controller;
 
 import com.example.core1.dao.TimeSheetRepo;
 import com.example.core1.domain.TimeSheetForSummary;
+import com.example.core1.domain.User;
+import com.example.core1.domain.EmergencyContact;
+import com.example.core1.domain.TimeSheet;
 import com.example.core1.service.ITestService;
 import com.example.core1.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +45,31 @@ public class TestController {
         }
         System.out.println(list.toString());
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(@RequestParam Map<String, String> username) {
+        String uname = username.get("userName");
+        List<TimeSheet> timeSheetList = new ArrayList<>();
+        List<EmergencyContact> emergencyContactList = new ArrayList<>();
+        EmergencyContact ec1 = new EmergencyContact("Daniel", "Lee", "2223334456");
+        EmergencyContact ec2 = new EmergencyContact("Joseph", "Parker", "1112223355");
+        emergencyContactList.add(ec1);
+        emergencyContactList.add(ec2);
+        User user = new User("60dfe7706d29da8cbbdb82f6", "Zack", "123456", "1234567890", "zack@beaconfire.com", "200 Sayre Drive, Princeton, New Jersey, 08648", emergencyContactList, timeSheetList);
+
+        if (user == null) {
+            System.out.println("No profile available");
+        }
+        System.out.println(user.toString());
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping(value="/updateprofile")
+    public ResponseEntity<Map<String, Object>> updateUserProfile(@RequestParam Map<String, Object> params){
+        System.out.println(params);
+        Map<String, Object> resultMap = iUserService.getAllUsers();
+        return ResponseEntity.ok(resultMap);
     }
 
     @GetMapping("/testGet")
